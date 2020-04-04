3803189b4d8a12810342a3ab9e7647429293af23
Julien <julien.tanay@gmail.com>
2020-04-04 16:54:47 +0200

    :sparkles: reset on stop

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "@emotion/styled/macro";
 import tw from "twin.macro";
 import ReactPlayer from "react-player";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStop, faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const Wrapper = styled.div`
   ${tw`w-32
@@ -10,16 +12,25 @@ const Wrapper = styled.div`
   rounded
   border
   border-white
-  cursor-pointer`}
+  cursor-pointer
+  relative`}
   ${(props) => props.isPlaying && tw`bg-white text-wikiwars-blue`}
 `;
 
 export default ({ name, url, category = "" }) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const playerEl = useRef(null);
   return (
-    <Wrapper isPlaying={isPlaying} onClick={() => setIsPlaying(!isPlaying)}>
-      <div>{name}</div>
+    <Wrapper
+      isPlaying={isPlaying}
+      onClick={() => {
+        playerEl.current.seekTo(0);
+        setIsPlaying(!isPlaying);
+      }}
+    >
+      <span>{name}</span>
       <ReactPlayer
+        ref={playerEl}
         url={url}
         width="100%"
         height="100%"
@@ -27,6 +38,9 @@ export default ({ name, url, category = "" }) => {
         loop
         playsinline
       />
+      <div tw="absolute bottom-0 right-0 mb-2 mr-2">
+        <FontAwesomeIcon icon={isPlaying ? faStop : faPlay} />
+      </div>
     </Wrapper>
   );
 };
